31166cfcb738454ffae8bae0a758b75ead40ef09
NamaWorks <pedronayamartinez@hotmail.com>
2024-02-27 22:20:23 +0100

    add fixes in hangman and prepare endgame texts

--- src/components/games/hangman/gameplay-hm.js
+++ src/components/games/hangman/gameplay-hm.js
@@ -3,12 +3,14 @@ import { data } from "../../../data/data";
 import { createTextLine } from "../../single_functions/create-text-line";
 import { printInCommandLine } from "../../single_functions/print-in-commandline";
 import { inputMain } from "../../../data/constant-variables";
+import { endGameMessage } from "../../single_functions/general-messages";
 
 const wordsList = data.texts.textsGames.hangMan.inGameTexts.wordsToGuess
 let wordsListLength = wordsList.length
 sessionStorage.setItem("selectedWord", "")
 sessionStorage.setItem("selectedWordArr", "")
 sessionStorage.setItem("dashesWord", "")
+sessionStorage.setItem("triesLeft", 6)
 
 export const getRandomWord = () => {
   let randomInteger = getRandomInteger(0, wordsListLength -1)
@@ -24,11 +26,22 @@ let stringDashes = JSON.stringify(dashesWord)
 sessionStorage.setItem("dashesWord", stringDashes)
 }
 
-export const printDashesWord = (arr) => {
+export const printDashesWord = () => {
+  let triesLeft = sessionStorage.getItem("triesLeft")
+
+  let dashesToPrint = JSON.parse(sessionStorage.getItem("dashesWord"))
+
+
   let printDashesWordsText = createTextLine("games-hub", "this is the word you have to guess:")
   printInCommandLine(printDashesWordsText)
-  let lineDashesWord = createTextLine("games-hub", arr)
+
+
+  let lineDashesWord = createTextLine("games-hub", dashesToPrint)
   printInCommandLine(lineDashesWord)
+
+  let lineTriesleft = createTextLine("games-hub", `you have ${sessionStorage.getItem("triesLeft")} tries left`)
+  printInCommandLine(lineTriesleft)
+
 }
 
 
@@ -42,11 +55,21 @@ export const askForLetter = () => {
 const letterSelection = (event) => {
   let selectedWord = sessionStorage.getItem("selectedWord")
   const inputMain = document.querySelector("#input-main");
+  let triesLeft = sessionStorage.getItem("triesLeft")
   if(event.code === "Enter") {
     if(selectedWord.includes(inputMain.value)){
       changeDashForLetter(inputMain.value)
+    } else {
+      if (triesLeft > 0){
+      let modifiedtriesLeft = triesLeft - 1
+      sessionStorage.setItem("triesLeft", modifiedtriesLeft)
+      changeDashForLetter(inputMain.value)
+      } else if (triesLeft = 0) {
+        loseMatch()
+        // inputMain.removeEventListener("keydown", function addAskForLetterFunction (event) {letterSelection(event);})
+      }
     }
-  }  
+  }
 }
 
 const changeDashForLetter = (letter) => {
@@ -66,4 +89,14 @@ const changeDashForLetter = (letter) => {
   let dashesWordTest = sessionStorage.getItem("dashesWord")
   
   printDashesWord(dashesWordToArr)
+}
+
+const loseMatch = () => {
+  let loosingMessageLine = createTextLine("games-hub", `you ran out of tries D:`)
+  printInCommandLine(loosingMessageLine)
+
+  let theWordToBeGuessed = createTextLine("games-hub", `the word was: ${sessionStorage.getItem("selectedWord")}`)
+  printInCommandLine(theWordToBeGuessed)
+
+  endGameMessage()
 }
